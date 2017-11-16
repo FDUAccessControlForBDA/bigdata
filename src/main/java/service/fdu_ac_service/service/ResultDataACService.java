225addff926791084b7c5c7b7e0666f56c3cadc0
symsimmy <1239347105@qq.com>
2017-11-16 10:49:02 +0800

    fix bug:通过操作的逻辑错误;

--- src/main/java/service/fdu_ac_service/service/ResultDataACService.java
+++ src/main/java/service/fdu_ac_service/service/ResultDataACService.java
@@ -138,10 +138,14 @@ public class ResultDataACService {
                         }
                         if(ret>0){
                             return 1;
+                        }else{
+                            return 0;
                         }
                     }
                 }
             }
+            //如果不是全部同意,但是当前同意操作成功完成,返回1
+            return 1;
         }
         return 0;
 
